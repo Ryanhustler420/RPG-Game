988cc563604e6370ffaeb8158a3d64e0383e087b
Gaurav Gupta <gouravgupta840@gmail.com>
2020-07-12 21:37:49 +0530

    Moving Player With NavMeshAgent Component

---
+++ DiabloLite/Assets/Scripts/Mover.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    /* fields for this class */
+    [SerializeField]    Transform       target;
+    private             NavMeshAgent    navMesh;
+
+    void Awake()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMesh.destination = target.position;
+        // navMesh.SetDestination(target.position); // same
+    }
+}
