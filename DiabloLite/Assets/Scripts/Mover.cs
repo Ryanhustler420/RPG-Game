75afe3ebd05368cab561f38ed584360761c62dad
Gaurav Gupta <gouravgupta840@gmail.com>
2020-07-12 23:56:22 +0530

    Click To Move

--- DiabloLite/Assets/Scripts/Mover.cs
+++ DiabloLite/Assets/Scripts/Mover.cs
@@ -3,27 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Internal;
 
 public class Mover : MonoBehaviour
 {
     /* fields for this class */
     [SerializeField]    Transform       target;
     private             NavMeshAgent    navMesh;
-    private             Ray             lastRay;
 
     void Awake()
     {
         navMesh = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            navMesh.destination = target.position;
+            MoveToCursor();
         }
-        Debug.DrawRay(lastRay.origin, lastRay.direction * 100f);
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if(hasHit)
+        {
+            navMesh.destination = hit.point;
+        }
     }
 }
