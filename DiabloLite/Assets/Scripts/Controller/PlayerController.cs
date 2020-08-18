d42bdeab5d395f152d373ec8a7a6ffce7d2ed2c7
Gaurav Gupta <gouravgupta840@gmail.com>
2020-08-18 20:03:27 +0530

    Namespaces To See Dependencies
    To Prevent From Dependencies Hell

--- DiabloLite/Assets/Scripts/Controller/PlayerController.cs
+++ DiabloLite/Assets/Scripts/Controller/PlayerController.cs
@@ -1,26 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using RPG.Movement;
 
-public class PlayerController : MonoBehaviour
+namespace RPG.Controller
 {
-    // Update is called once per frame
-    void Update()
+    public class PlayerController : MonoBehaviour
     {
-        if (Input.GetMouseButton(0))
+        // Update is called once per frame
+        void Update()
         {
-            MoveToCursor();
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
         }
-    }
 
-    private void MoveToCursor()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if (hasHit)
+        private void MoveToCursor()
         {
-            GetComponent<Mover>().MoveTo(hit.point);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(ray, out hit);
+            if (hasHit)
+            {
+                GetComponent<Mover>().MoveTo(hit.point);
+            }
         }
     }
 }
--- DiabloLite/Assets/Scripts/Movement/Mover.cs
+++ DiabloLite/Assets/Scripts/Movement/Mover.cs
@@ -1,36 +1,38 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+namespace RPG.Movement
 {
-
-    void Update()
+    public class Mover : MonoBehaviour
     {
-        UpdateAnimator();
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void MoveTo(Vector3 desctination)
+        {
+            GetComponent<NavMeshAgent>().destination = desctination;
+        }
+
+        private void UpdateAnimator()
+        {
+            // for turning the charcter into forward direction
+            // tweek from navMess Agent from inspector
+
+            // NOTE: velocity means speed and direction
+            // navMash gives use speed and direction but it won't gives us when turning command like take left or right at this moment.
+            // so we need to convert that vactor3 into local vector3
+
+            // want to get vector3 from navMash which has already direction and speed.
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            // convert that to local velocity, which helps our animator blend tree to understand where the speed decreases.
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            // get the forward or (z) direction speed,
+            float speed = localVelocity.z;
+            // updating speed every frame and setting to blend tree
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+
     }
-
-    public void MoveTo(Vector3 desctination)
-    {
-        GetComponent<NavMeshAgent>().destination = desctination;
-    }
-
-    private void UpdateAnimator()
-    {
-        // for turning the charcter into forward direction
-        // tweek from navMess Agent from inspector
-
-        // NOTE: velocity means speed and direction
-        // navMash gives use speed and direction but it won't gives us when turning command like take left or right at this moment.
-        // so we need to convert that vactor3 into local vector3
-
-        // want to get vector3 from navMash which has already direction and speed.
-        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
-        // convert that to local velocity, which helps our animator blend tree to understand where the speed decreases.
-        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
-        // get the forward or (z) direction speed,
-        float speed = localVelocity.z;
-        // updating speed every frame and setting to blend tree
-        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
-    }
-
 }
